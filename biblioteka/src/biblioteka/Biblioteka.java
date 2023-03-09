0b51b940642c35c5446e2702c967d24186422a12
MiraDimitrijevic <marinadimitrijevic03@gmail.com>
2023-03-09 09:11:22 +0100

    Napravljena klasa Biblioteka.

---
+++ biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,40 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	
+	private List<Knjiga> knjige= new ArrayList<Knjiga>();
+
+	@Override
+	public void dodajknjigu(Knjiga knjiga) {
+knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		List<Knjiga> listaRezultati= new ArrayList<>();
+		for(Knjiga k :knjige) {
+			if(k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim())) {
+				listaRezultati.add(k);
+			}
+		}
+		return listaRezultati;
+	}
+
+}
